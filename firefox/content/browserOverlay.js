59c267c426d20eea2dcfb5ffb22aee0ffaff73a5
Michael Lawrence <mikeklawrence@gmail.com>
2012-07-18 08:03:00 -0700

    Fixed Firefox bug where target node is not <A>, by walking up tag tree from target node until an <A> node is found.

--- firefox/content/browserOverlay.js
+++ firefox/content/browserOverlay.js
@@ -16,16 +16,15 @@ if ("undefined" == typeof(Unsocialize)) {
  */
 window.addEventListener("load", function(loadEvent) {
   document.addEventListener("contextmenu", function(contextEvent) {
-    if (contextEvent.target.tagName == "A") {
-      Unsocialize.linkURL = contextEvent.target.href;
+    // Walks up the DOM tree up to maxDepth levels looking for an <A> element
+    var maxDepth = 10;
+    var target = contextEvent.target;
+    for (var i = 0; i < maxDepth && target.nodeName != "A"; i++) {
+      target = target.parentElement;
+    }
+    if (target.nodeName == "A") {
+      Unsocialize.linkURL = target.href;
       document.getElementById("unsocialize-menu-item").hidden = "";
-
-    } else if (contextEvent.target.tagName == "SPAN" &&
-               contextEvent.target.parentElement.tagName == "A") {
-      // Sometimes there will be a span wrapped in an <A> element.
-      Unsocialize.linkURL = contextEvent.target.parentElement.href;
-      document.getElementById("unsocialize-menu-item").hidden = "";
-
     } else {
       document.getElementById("unsocialize-menu-item").hidden = "true";
     }
